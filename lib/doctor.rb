f6107bb33d35f10f9e5dcdad7cb0915053a471c7
Pbsmith82 <pbsmith82@gmail.com>
2020-10-04 14:18:34 -0400

    Artist-Genres, Doctor-Patients

---
+++ lib/doctor.rb
@@ -0,0 +1,35 @@
+class Doctor
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select {|item| item.doctor == self}
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        pt = []
+        Appointment.all.each do |item|
+            if item.doctor == self
+                pt << item.patient
+            end
+        end
+        pt
+    end
+
+
+
+end
