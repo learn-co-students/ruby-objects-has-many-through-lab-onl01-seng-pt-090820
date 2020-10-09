8bc3894cb5bf5e5b595e6729dc4254a99f6eff41
Christian Simon <csimon1127@gmail.com>
2020-10-09 19:26:00 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+class Artist 
+  attr_reader :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    song.artist = self 
+    Song.all << song
+    song
+  end 
+  
+  def songs 
+    Song.all.select { |song|
+      song.artist == self
+    }
+  end
+  
+  def genres 
+    songs.collect { |song|
+      song.genre
+    }
+  end
+  
+  
+  
+end
